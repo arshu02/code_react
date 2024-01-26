a34f2d7199429c384c93efa511d08ef798d530f2
arshu02 <arshusingh26@gmail.com>
2024-01-26 14:11:52 +0530

    create element

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,8 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function Myapp() {
+  return (
+    <div>
+      <h1>just checking !</h1>
+    </div>
+  );
+}
+const anotherElement = (
+  <a href="https://google.com" target="_blank">
+    visit google
+  </a>
 );
+const reactelement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "click me to visit google"
+);
+ReactDOM.createRoot(document.getElementById("root")).render(reactelement);
